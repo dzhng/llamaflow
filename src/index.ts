1fc7ac69114549f49a0293ab7b8bc08206346e33
David Zhang <dzz0615@gmail.com>
2023-04-20 16:29:42 -0700

    also exporting text splitters as utility fn

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import * as prompt from './prompts';
 export type { Chat, SplitRequestFn } from './chat';
 export * from './models/errors';
 export * from './models/openai';
+export * from './text-splitter';
 export * from './types';
 export { prompt };
